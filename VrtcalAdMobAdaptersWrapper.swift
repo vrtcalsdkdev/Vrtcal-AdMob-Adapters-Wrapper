0120fba692fae408a4b6b87d19aae5941f03d3a2
Scott McCoy <scott.mccoy@vrtcal.com>
2024-08-27 17:07:55 -0700

    Added Dennis' iPad

--- VrtcalAdMobAdaptersWrapper.swift
+++ VrtcalAdMobAdaptersWrapper.swift
@@ -39,8 +39,9 @@ class VrtcalAdMobAdaptersWrapper: NSObject, AdapterWrapperProtocol {
         
         
         GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [
-            "bc8b2db6f176d262669c7768ea6ea2e5", // Vrtcal iPhone 11
-            "bd799f83fd95f07a2719d1bb226c76ca"  // Vrtcal iPhone XR
+            "bc8b2db6f176d262669c7768ea6ea2e5", // Scott Vrtcal iPhone 11
+            "bd799f83fd95f07a2719d1bb226c76ca",  // Scott Vrtcal iPhone XR
+            "54829330cdb5c96af35c5e9477a3ee6c" // Dennis Vrtcal iPad
         ]
 
         GADMobileAds.sharedInstance().start { gadInitializationStatus in
